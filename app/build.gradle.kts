d0574fd165863087c7c24c034e401c0098ebeba7
Amadou Iyawa <amadiyawa@yahoo.com>
2024-05-08 15:12:00 +0100

    importing my feature's base module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+rootProject.name = "IncubaTor"
+
+include(
+    ":app",
+    ":feature_base"
+)
+
 pluginManagement {
     repositories {
         google {
@@ -11,13 +18,19 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        mavenLocal()
         mavenCentral()
     }
 }
 
-rootProject.name = "IncubaTor"
-include(":app")
+// Enables the feature preview for type-safe project accessors in Gradle.
+// Type-safe project accessors provide a type-safe and refactoring-friendly way to access other projects.
+// This feature is particularly useful in a multi-module project setup, where you often need to declare dependencies on other projects.
+// By enabling this feature, you can access other projects in your settings.gradle.kts file in a type-safe manner.
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
